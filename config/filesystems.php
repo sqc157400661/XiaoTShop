9c809a7d4e4e830207825ddfb2f5668f31794dd9
Sqc <sqc157400661@163.com>
2020-11-02 11:36:19 +0800

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,7 @@ return [
             //'endpoint_internal' => '<internal endpoint [OSS内网节点] 如：oss-cn-shenzhen-internal.aliyuncs.com>', // v2.0.4 新增配置属性，如果为空，则默认使用 endpoint 配置(由于内网上传有点小问题未解决，请大家暂时不要使用内网节点上传，正在与阿里技术沟通中)
             //'cdnDomain'     => '<CDN domain, cdn域名>', // 如果isCName为true, getUrl会判断cdnDomain是否设定来决定返回的url，如果cdnDomain未设置，则使用endpoint来生成url，否则使用cdn
 //            'debug'         => true,
-            'url' => 'https://xiaot-static.oss-cn-hangzhou.aliyuncs.com'
+            'url' => 'https://xit-staticxxx.oss-cn-hangzhou.aliyuncs.com'
         ],
 
         's3' => [
